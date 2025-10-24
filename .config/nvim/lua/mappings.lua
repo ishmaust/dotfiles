4befdc446cf002824587f7d1619ba3129e5e3ac4
ishmaust <ishmaust@gmail.com>
2025-10-25 01:01:26 +0200

    Update configuration

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,5 +6,9 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "H", "^")
+map("n", "L", "$")
+map("n", "J", "}")
+map("n", "K", "{")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
